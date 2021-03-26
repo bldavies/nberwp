1a7fe2ae678ef5c303870c63b4321e430447a1f4
Ben Davies <git@bldavies.fastmail.com>
2021-03-26 15:55:57 -0700

    Add program categories

--- R/program_descriptions.R
+++ R/program_descriptions.R
@@ -1,6 +1,6 @@
 #' NBER working paper program descriptions
 #'
-#' Crosswalk between NBER program codes and descriptions.
+#' Crosswalk between NBER program codes, descriptions and categories.
 #'
 #' @docType data
 #'
@@ -10,7 +10,13 @@
 #' \describe{
 #' \item{program}{Program code}
 #' \item{program_desc}{Program description}
+#' \item{program_category}{Program category based on Chari and Goldsmith-Pinkham (2017)}
 #' }
+#'
+#' @references Chari, A. and P. Goldsmith-Pinkham (2017).
+#' Gender Representation in Economics Across Topics and Time: Evidence from the NBER Summer Institute.
+#' NBER Working Paper No. 23953,
+#' National Bureau of Economic Research.
 #'
 #' @source \href{https://www.nber.org/RePEc/nbr/nberwo/}{National Bureau of Economic Research}
 "program_descriptions"
